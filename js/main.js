17b8f76f216c56aea3249ec686cb5439dc10bfc7
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-20 15:42:06 +0100

    bug cost ticket (NaN)

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+const buttonCreate = document.getElementById("create");
+buttonCreate.addEventListener("click",
+    function () {
+        let userName = document.getElementById("userName");
+        let userKm = document.getElementById("km");
+        let userAge = parseInt(document.getElementById("eta"));
+
+        // CONTROLLO ATTRIBUTI PASSEGGERO
+        console.log(userName.value);
+        console.log(km.value);
+        console.log(eta.value);
+
+        document.getElementById("user").innerHTML = userName.value;
+        
+        let total = 0;
+        let price = (userKm * 0.21).toFixed(2);
+        if (userAge >= 65) {
+            let discount = ((price * 40) / 100).toFixed(2);
+            total = (price - discount).toFixed(2);
+            document.getElementById("ticket").innerHTML = "Biglietto Sconto Over 65";
+        } else if (userAge < 18) {
+            let discount = ((price * 20) / 100).toFixed(2);
+            total = (price - discount).toFixed(2);
+            document.getElementById("ticket").innerHTML = "Biglietto Sconto under 18";
+        } else {
+            total = price;
+            console.log(total);
+            document.getElementById("ticket").innerHTML = "Biglietto standard";
+        }
+
+        document.getElementById("price").innerHTML = total;
+    }
+)
